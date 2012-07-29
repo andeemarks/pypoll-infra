6dd21bb5d6526ace467ce9c6c3b8bebac3b79262
Andy Marks <amarks@mini.local>
2012-07-29 09:38:29 +0800

    First commit

---
+++ manifest.pp
@@ -0,0 +1,32 @@
+node "default" {
+        include python
+        include django
+}
+
+class python {
+        package {
+                "build-essential": ensure => latest;
+                "python": ensure => "2.7.2-7ubuntu2";
+                "python-dev": ensure => "2.7.2-7ubuntu2";
+                "python-setuptools": ensure => installed;
+        }
+        exec { 
+                "pip":
+                command => "sudo easy_install pip",
+                path => "/usr/local/bin:/usr/bin:/bin",
+                refreshonly => true,
+                logoutput => "on_failure",
+                require => Package["python-setuptools"],
+                subscribe => Package["python-setuptools"];
+        }
+}
+
+class django {
+        package {
+                "django":
+                        ensure => "1.4",
+                        provider => pip,
+                        require => Exec["pip"];
+                }
+}
+
